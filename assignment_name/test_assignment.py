ca3e3043f4dcb1912157cccd0fe2482a99862699
Chase Mateusiak <chase.mateusiak@gmail.com>
2023-09-08 16:36:39 -0500

    Update test_assignment.py

--- assignment_name/test_assignment.py
+++ assignment_name/test_assignment.py
@@ -1,5 +1,6 @@
 import logging
 import unittest
+from gradescope_utils.autograder_utils.decorators import weight
 from cse587Autils.configure_logging import configure_logging
 from assignment_name.assignment import square
 
@@ -8,6 +9,7 @@ configure_logging(logging.WARN)
 
 class Testassignment_name(unittest.TestCase):
     
+    @weight(10)
     def test_square(self):
         
         # Test with an empty list
